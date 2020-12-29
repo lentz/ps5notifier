738558b315e8a09e8d0eac1489b3d4c7b340c2f2
Jesse Lentz <jesselentz@gmail.com>
2020-12-29 11:10:16 -0500

    Update sender from

--- index.js
+++ index.js
@@ -11,35 +11,35 @@ async function checkStock(store, url, xpath, page) {
     await page.goto(url);
     const inStock = await page.waitForXPath(xpath, { visible: true });
   } catch (err) {
-    if (!/Navigation timeout/i.test(err.message)) {
+    if (/Navigation timeout/i.test(err.message)) {
       console.log('Navigation timeout');
-      return;
+    } else {
+      console.log(err.stack);
+      console.log('IN STOCK AT', store);
+      const screenshot = await page.screenshot({ encoding: 'base64' });
+      await page.browser().close();
+
+      await sgMail.send({
+        attachments: [{
+          filename: 'screenshot.png',
+          type: 'image/png',
+          content_id: 'screenshot',
+          content: screenshot,
+          disposition: 'inline',
+        }],
+        from: 'PS5 Notifier <fantasynotify@mailinator.com>',
+        html: `${url}<br /><br /><img src="cid:screenshot" />`,
+        subject: `PS5 in stock at ${store}`,
+        to: process.env.EMAIL,
+      });
+
+      process.exit();
     }
-
-    console.log('IN STOCK AT', store);
-    const screenshot = await page.screenshot({ encoding: 'base64' });
-    await page.browser().close();
-
-    await sgMail.send({
-      attachments: [{
-        filename: 'screenshot.png',
-        type: 'image/png',
-        content_id: 'screenshot',
-        content: screenshot,
-        disposition: 'inline',
-      }],
-      from: 'PS5 Notifier <noreply@ps5notifier.herokuapp.com>',
-      html: `${url}<br /><br /><img src="cid:screenshot" />`,
-      subject: `PS5 in stock at ${store}`,
-      to: process.env.EMAIL,
-    });
-
-    process.exit();
   }
 }
 
 (async () => {
-  const browser = await puppeteer.launch({ defaultViewport: { width: 1366, height: 1024 } });
+  const browser = await puppeteer.launch({ defaultViewport: { width: 1280, height: 800 } });
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36');
 
@@ -74,4 +74,4 @@ async function checkStock(store, url, xpath, page) {
 
     await page.waitForTimeout(30000);
   }
-})().catch(console.error);
+})().catch((err) => console.error(err));
