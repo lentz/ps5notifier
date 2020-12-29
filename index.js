7887fa5a4b62f31a245e4381604ea38e09e0947e
Jesse Lentz <jesselentz@gmail.com>
2020-12-29 16:12:18 -0500

    Fix typo

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function checkStock(store, url, xpath, page) {
         from: 'PS5 Notifier <fantasynotify@mailinator.com>',
         html: `${url}<br /><br /><img src="cid:screenshot" />`,
         subject: `PS5 in stock at ${store}`,
-        to: process.env.EMAIL.split(','),
+        to: process.env.EMAILS.split(','),
       });
 
       process.exit();
