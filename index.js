743899cc4142f6db5c01fb8d2484720664114fb1
Jesse Lentz <jesselentz@gmail.com>
2021-01-07 16:27:16 -0500

    Add GameStop and abstract stores into array

--- index.js
+++ index.js
@@ -5,99 +5,109 @@ require('dotenv').config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-async function checkStock(store, url, xpath, page) {
-  try {
-    console.log('Checking', store);
-    await page.goto(url);
-    const inStock = await page.waitForXPath(xpath, { timeout: 10000, visible: true });
-  } catch (err) {
-    if (/waiting for XPath/i.test(err.message)) {
-      console.log(err.stack);
-      console.log('IN STOCK AT', store);
-      const screenshot = await page.screenshot({ encoding: 'base64' });
-      await page.browser().close();
-
-      await sgMail.send({
-        attachments: [{
-          filename: 'screenshot.png',
-          type: 'image/png',
-          content_id: 'screenshot',
-          content: screenshot,
-          disposition: 'inline',
-        }],
-        from: 'PS5 Notifier <fantasynotify@mailinator.com>',
-        html: `${url}<br /><br /><img src="cid:screenshot" />`,
-        subject: `PS5 in stock at ${store}`,
-        to: process.env.EMAILS.split(','),
-      });
-
-      process.exit();
-    } else {
-      console.log('Error checking stock:', err);
-    }
-  }
-}
+const stores = [
+  {
+    delay: 0,
+    name: 'Costco Disc',
+    url: 'https://www.costco.com/sony-playstation-5-gaming-console-bundle.product.100691489.html',
+    xpath: '//img[@title="Out of Stock"]',
+  },
+  {
+    delay: 0,
+    name: 'Bestbuy Disc',
+    url: 'https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149',
+    xpath: '//button[text() = "Sold Out"]',
+  },
+  {
+    delay: 0,
+    name: 'Bestbuy Digital',
+    url: 'https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161',
+    xpath: '//button[text() = "Sold Out"]',
+  },
+  {
+    delay: 0,
+    name: 'Target Disc',
+    url: 'https://www.target.com/p/playstation-5-console/-/A-81114595',
+    xpath: '//div[@data-test="notAvailableForShippingMessage"]',
+  },
+  {
+    delay: 0,
+    name: 'Target Digital',
+    url: 'https://www.target.com/p/playstation-5-digital-edition-console/-/A-81114596',
+    xpath: '//div[@data-test="notAvailableForShippingMessage"]',
+  },
+  {
+    delay: 0,
+    name: 'Amazon Disc',
+    url: 'https://www.amazon.com/dp/B08FC5L3RG',
+    xpath: '//div[@id="outOfStock" or @id="unqualifiedBuyBox_feature_div"]',
+  },
+  {
+    delay: 0,
+    name: 'Amazon Digital',
+    url: 'https://www.amazon.com/dp/B08FC6MR62',
+    xpath: '//div[@id="outOfStock" or @id="unqualifiedBuyBox_feature_div"]',
+  },
+  {
+    delay: 0,
+    name: 'GameStop Disc',
+    url: 'https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5/B225169X.html',
+    xpath: '//button[text() = "Not Available"]',
+  },
+  {
+    delay: 0,
+    name: 'GameStop Digital',
+    url: 'https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5-digital-edition/B225171P.html',
+    xpath: '//button[text() = "Not Available"]',
+  },
+];
 
 (async () => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+    defaultViewport: { width: 1280, height: 800 },
+  });
+
   while(true) {
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox'],
-      defaultViewport: { width: 1280, height: 800 },
-    });
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36');
 
-    // await checkStock(
-    //   'Costco Disc',
-    //   'https://www.costco.com/sony-playstation-5-gaming-console-bundle.product.100691489.html',
-    //   '//img[@title="Out of Stock"]',
-    //   page,
-    // );
+    for (const store of stores) {
+      if (store.delay < Date.now()) {
+        try {
+          console.log('Checking', store.name);
+          await page.goto(store.url);
+          const inStock = await page.waitForXPath(store.xpath, { timeout: 10000, visible: true });
+        } catch (err) {
+          if (/waiting for XPath/i.test(err.message)) {
+            console.log(err.stack);
+            console.log('IN STOCK AT', store.name);
+            const screenshot = await page.screenshot({ encoding: 'base64' });
 
-    await checkStock(
-      'Bestbuy Disc',
-      'https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149',
-      '//button[text() = "Sold Out"]',
-      page,
-    );
+            await sgMail.send({
+              attachments: [{
+                filename: 'screenshot.png',
+                type: 'image/png',
+                content_id: 'screenshot',
+                content: screenshot,
+                disposition: 'inline',
+              }],
+              from: 'PS5 Notifier <fantasynotify@mailinator.com>',
+              html: `${store.url}<br /><br /><img src="cid:screenshot" />`,
+              subject: `PS5 in stock at ${store.name}`,
+              to: process.env.EMAILS.split(','),
+            });
 
-    await checkStock(
-      'Bestbuy Digital',
-      'https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161',
-      '//button[text() = "Sold Out"]',
-      page,
-    );
+            store.delay = Date.now() + (1000 * 60 * 60);
+          } else {
+            console.log('Error checking stock:', err);
+          }
+        }
+      }
+    }
 
-    await checkStock(
-      'Target Disc',
-      'https://www.target.com/p/playstation-5-console/-/A-81114595',
-      '//div[@data-test="notAvailableForShippingMessage"]',
-      page,
-    );
-
-    await checkStock(
-      'Target Digital',
-      'https://www.target.com/p/playstation-5-digital-edition-console/-/A-81114596',
-      '//div[@data-test="notAvailableForShippingMessage"]',
-      page,
-    );
-
-    await checkStock(
-      'Amazon Disc',
-      'https://www.amazon.com/dp/B08FC5L3RG',
-      '//div[@id="outOfStock" or @id="unqualifiedBuyBox_feature_div"]',
-      page,
-    );
-
-    await checkStock(
-      'Amazon Digital',
-      'https://www.amazon.com/dp/B08FC6MR62',
-      '//div[@id="outOfStock" or @id="unqualifiedBuyBox_feature_div"]',
-      page,
-     );
-
-    console.log('Sleeping');
+    console.log('Sleeping', new Date().toLocaleString());
     await page.waitForTimeout(30000);
-    await browser.close();
+    await page.close();
   }
 })().catch((err) => console.error(err));
